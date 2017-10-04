844ff60aabdc8ce36f5ab4ad70e90054b0aa3f7a
brianflores <brianf_lp@hotmail.com>
2017-10-04 19:20:57 -0300

    Agrego localtunnel

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ if [[ `id -u` -ne 0 ]] ; then echo "Correr con sudo" ; exit 1 ; fi
 apt install -y nodejs npm libopencv-dev libvips
 
 npm install -g bower
+npm install -g localtunnel
 
 npm install
 bower install --allow-root
